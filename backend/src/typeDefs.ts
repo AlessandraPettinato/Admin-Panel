f5f5f88b2a03c6e91003e203d1bbdf08d991e44b
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-11-22 16:52:21 +0100

    feat: declare typeDefs as per instruction. Define new scalar Date

--- backend/src/typeDefs.ts
+++ backend/src/typeDefs.ts
@@ -1,6 +1,33 @@
 const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
+scalar Date
+
+type Policy {
+  customer: [Customer]
+  provider: String
+  insuranceType: [InsuranceType]
+  status: [PolicyStatus]
+  policyNumber: String
+  startDate: Date
+  endDate: Date
+  createdAt: Date
+}
+
+type Customer {
+  firstName: String
+  lastName: String
+  dateOfBirth: Date
+}
+
+type InsuranceType {
+  insuranceType: String
+}
+
+type PolicyStatus {
+  policyStatus: String
+}
+
   type Book {
     title: String
     author: String
