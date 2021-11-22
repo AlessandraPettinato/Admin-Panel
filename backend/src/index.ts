2a4ab32a52d73fd6f5459ba318922c96ae52c716
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-11-22 16:41:33 +0100

    feat: include database connection. Stored user and passw into env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,31 @@
 import { ApolloServer } from "apollo-server-express";
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 const typeDefs = require("./typeDefs.ts");
 const resolvers = require("./resolvers.ts");
 
+const databaseURL = "mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}0.0jhze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(
+  databaseURL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => console.log("Database connected")
+);
+
 const startServer = async () => {
-  const app = express(); 
+  const app = express();
 
-  const apolloServer = new ApolloServer({typeDefs, resolvers}); 
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-  await apolloServer.start(); 
-  apolloServer.applyMiddleware({app}); 
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => console.log(`Server up and running on port 4000${apolloServer.graphqlPath}`))
 };
 
-startServer()
+startServer();
