5e34b5bd1cb02350a1f8eb49f9cd7f19e2150bb4
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-11-23 16:17:12 +0100

    style: fix styles issue in table. Need further styling

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -20,11 +20,11 @@ const Table: React.FC = () => {
 
 	return (
 		<>
-			<div className="flex flex-col">
+			<div className="flex flex-col ">
 				<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 					<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
 						<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"></div>
-						<table className="min-w-full divide-y divide-gray-200">
+						<table className="min-w-full divide-y divide-gray-200 border-collapse border">
 							<thead className="bg-gray-50">
 								<tr>
 									<th
