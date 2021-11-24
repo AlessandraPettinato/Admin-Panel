1b733443ccf9d88c9cbcd495eb583f0e147681a5
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-11-24 17:48:27 +0100

    fix: correct bugs that prevented query to execution. Issue was in incorrect async operation for db connection

--- backend/src/dbconnection.ts
+++ backend/src/dbconnection.ts
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const databaseURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster.zuufe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const databaseURL = `mongodb+srv://Sandra:Tokyo2018@cluster.kveob.mongodb.net/Data?retryWrites=true&w=majority`
 
-const dbconnection = async () => {
-  const connection = await mongoose.connect(databaseURL,  { useNewUrlParser: true, useUnifiedTopology: true }); 
-
-  return connection
-}
+const dbconnection = mongoose.connect(databaseURL,  { useNewUrlParser: true, useUnifiedTopology: true }); 
 
   module.exports = dbconnection;
