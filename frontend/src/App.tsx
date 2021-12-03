1cdce540473beaae0f8cf1ef874d9e7878a0fd3e
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-12-03 13:12:20 +0100

    style: add login-ui on client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import "./index.css";
 import Provider from "./api/Provider";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
 
 const App: React.FC = () => {
 	return (
 		<>
 			<Provider>
-				<Dashboard />
+				<Login />
+				{/* <Dashboard /> */}
 			</Provider>
 		</>
 	);
