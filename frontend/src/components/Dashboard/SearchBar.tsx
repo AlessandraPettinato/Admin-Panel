2344be1ca26a4c192cbca2a25b6103695aa91cc1
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-12-08 16:35:17 +0100

    feat: import logout function from AuthContext and fire it onClick on logout button

--- frontend/src/components/Dashboard/SearchBar.tsx
+++ frontend/src/components/Dashboard/SearchBar.tsx
@@ -1,9 +1,13 @@
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useContext } from "react";
+import { AuthContext } from "../../context/auth-context";
+
 import { BiSearch } from "react-icons/bi";
 
 const SearchBar: React.FC<{
 	handleSearch: ChangeEventHandler<HTMLInputElement>;
 }> = ({ handleSearch }) => {
+	const { logout } = useContext(AuthContext);
+
 	return (
 		<nav className="bg-blue-400 pt-2 md:pt-1 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0">
 			<div className="flex flex-wrap items-center">
@@ -22,6 +26,7 @@ const SearchBar: React.FC<{
 					</div>
 				</div>
 				<button
+					onClick={logout}
 					className="bg-white text-blue-400 
 				hover:text-blue-600 font-semibold py-2 px-4 border border-blue-500 hover:border-blue-600 rounded mr-8"
 				>
