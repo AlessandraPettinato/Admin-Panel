3e1e7655d8d0181d4ea4d29573b5c6e0c62b4dba
Alessandra Pettinato <alessandrapettinato@hotmail.it>
2021-12-18 13:56:18 +0100

    style: adjust ui of login component

--- frontend/src/components/Login/useStyles.tsx
+++ frontend/src/components/Login/useStyles.tsx
@@ -1,26 +1,23 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
+	backgroundImage: {
+		backgroundImage: "url(https://source.unsplash.com/random)",
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "cover",
+		backgroundPosition: "center",
+	},
 	box: {
-		margin: 8,
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-	},
-	avatar: {
-		margin: 1,
-		backgroundColor: "red",
-	},
-	login: {
-		fontFamily: "Nunito",
-	},
-	form: {
+		margin: "8rem 4rem 8rem 4rem",
 		display: "flex",
 		flexDirection: "column",
 		alignItems: "center",
 	},
 	button: {
 		marginBottom: "3rem",
+	},
+	container: {
+		height: "100vh",
 	},
 }));
 
